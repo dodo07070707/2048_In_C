58a3cc14bd582a7e38cfdf161e72f9010d3eaad4
DOYOON KIM <98579912+dodo07070707@users.noreply.github.com>
2023-11-16 14:53:47 +0900

    comment end, some ui fix

--- 2048inC/2048inC.c
+++ 2048inC/2048inC.c
@@ -21,12 +21,12 @@ char filenames5x5[25][10];
 int retry = 0;
 
 
-void gotoxy(int x, int y) {
+void gotoxy(int x, int y) { //커서 위치를 옮기는 함수
     COORD Pos = { x,y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
-void Nocursor()
+void Nocursor() // 커서를 안보이게 하는 함수
 {
     CONSOLE_CURSOR_INFO cursorInfo = { 0, };
     cursorInfo.dwSize = 1; //커서 굵기 (1 ~ 100)
@@ -34,29 +34,28 @@ void Nocursor()
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
-void _playSound(TCHAR* name) {
+void _playSound(TCHAR* name) { // 노래를 재생시키는 함수
     TCHAR command[100];
     wsprintf(command, TEXT("play %s"), name);
     mciSendString(command, NULL, 0, NULL);
 }
 
-//재생중이던 소리를 멈추기 위한 함수
-void _stopSound(wchar_t* name) {
+void _stopSound(wchar_t* name) { //노래를 멈추기 위한 함수
 
     wchar_t command[100];
     wsprintf(command, TEXT("stop %s"), name);
     mciSendString(command, NULL, 0, NULL);
 }
 
-char* _printRGBColoredString(int r, int g, int b, char* org) {
+char* _printRGBColoredString(int r, int g, int b, char* org) { // 글자색을 RGB값을 이용해 바꾸게 해주는 함수
     printf("\033[38;2;%d;%d;%dm%s\033[0m", r, g, b, org);
 }
 
-int random16() {
+int random16() { // 0~15중 한개의 수가 도출되게 하는 랜덤 함수
     return rand() % 16;
 }
 
-int random2or4() {
+int random2or4() { // 블록 생성 시 그 블록의 수를 정하는 랜덤 함수, 2 -> 90% / 4 -> 10%
     int randint = rand() % 10;
     if (randint == 0) {
         return 4;
@@ -64,7 +63,7 @@ int random2or4() {
     else return 2;
 }
 
-void _printtitle() {
+void _printtitle() { // 게임 초기 시작 시 제목을 보여주는 함수
     gotoxy(50, 5);
     printf("_______________      _____   ______   .__         _________  \n");
     gotoxy(50, 6);
@@ -83,7 +82,7 @@ void _printtitle() {
     system("cls");
 }
 
-void _printrules() {
+void _printrules() { // 규칙을 보여주는 함수
     gotoxy(52, 5);
     printf(".______       __    __   __       _______     _______.\n");
     gotoxy(52, 6);
@@ -114,7 +113,7 @@ void _printrules() {
 
 
 
-void _saveimagenames4x4() {
+void _saveimagenames4x4() { // 배열의 값에 따라 문자열 배열에 해당 값에 대응하는 사진의 이름을 저장하는 함수 (4x4용)
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             sprintf(filenames4x4[i*4+j], "%d.bmp", arr4[i][j]);
@@ -122,7 +121,7 @@ void _saveimagenames4x4() {
     }
 }
 
-void _saveimagenames5x5() {
+void _saveimagenames5x5() { // 배열의 값에 따라 문자열 배열에 해당 값에 대응하는 사진의 이름을 저장하는 함수 (5x5용)
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             sprintf(filenames5x5[i * 5 + j], "%d.bmp", arr5[i][j]);
@@ -130,7 +129,7 @@ void _saveimagenames5x5() {
     }
 }
 
-void DisplayImage(const char* filename, HDC hdc, int x, int y, int width, int height) {
+void DisplayImage(const char* filename, HDC hdc, int x, int y, int width, int height) { //이미지를 띄우는 함수
     // Convert ASCII filename to wide string
     int wfilename_length = MultiByteToWideChar(CP_ACP, 0, filename, -1, NULL, 0);
     wchar_t* wfilename = malloc(wfilename_length * sizeof(wchar_t));
@@ -155,7 +154,7 @@ void DisplayImage(const char* filename, HDC hdc, int x, int y, int width, int he
     }
 }
 
-void _displayimages4x4() {
+void _displayimages4x4() { // 파일명에 따라 이미지 띄우는 함수를 호출 + 사진 위치지정 하는 함수 (4x4용)
     HDC hdc = GetDC(GetConsoleWindow());
     int cnt = 0;
     int imageWidth = 150;
@@ -175,7 +174,7 @@ void _displayimages4x4() {
     ReleaseDC(GetConsoleWindow(), hdc);
 }
 
-void _displayimages5x5() {
+void _displayimages5x5() { // 파일명에 따라 이미지 띄우는 함수를 호출 + 사진 위치지정 하는 함수 (5x5용)
     HDC hdc = GetDC(GetConsoleWindow());
     int cnt = 0;
     int imageWidth = 150;
@@ -195,7 +194,7 @@ void _displayimages5x5() {
     ReleaseDC(GetConsoleWindow(), hdc);
 }
 
-int _printmain() {
+int _printmain() { //4x4를 플레이 할지, 5x5를 플레이할지 고르는 화면을 출력하는 함수
     gotoxy(51, 5);
     printf("                                                                        \n");
     gotoxy(51, 6);
@@ -270,7 +269,7 @@ int _printmain() {
 }
 
 
-void _resetcheckarr4x4() {
+void _resetcheckarr4x4() { //!
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             checkarr4[i][j] = 0;
@@ -286,15 +285,15 @@ void _resetcheckarr5x5() {
     }
 }
 
-void _scoreadd4x4(int n) {
+void _scoreadd4x4(int n) { // 플레이 중인 게임의 점수를 계산하는 함수 (4x4용)
     score4x4 = score4x4 + n;
 }
 
-void _scoreadd5x5(int n) {
+void _scoreadd5x5(int n) { // 플레이 중인 게임의 점수를 계산하는 함수 (5x5용)
     score5x5 = score5x5 + n;
 }
 
-void _logicw4x4() {
+void _logicw4x4() { // 플레이 중 위로가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (4x4용)
     while (1) {
         for (int i = 1; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -332,7 +331,7 @@ void _logicw4x4() {
     }
 }
 
-void _logicw5x5() {
+void _logicw5x5() { // 플레이 중 위로가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (5x5용)
     while (1) {
         for (int i = 1; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -370,7 +369,7 @@ void _logicw5x5() {
     }
 }
 
-void _logica4x4() {
+void _logica4x4() { // 플레이 중 왼쪽으로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (4x4용)
     while (1) {
         for (int i = 0; i < 4; i++) {
             for (int j = 1; j < 4; j++) {
@@ -408,7 +407,7 @@ void _logica4x4() {
     }
 }
 
-void _logica5x5() {
+void _logica5x5() { // 플레이 중 왼쪽으로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (5x5용)
     while (1) {
         for (int i = 0; i < 5; i++) {
             for (int j = 1; j < 5; j++) {
@@ -446,7 +445,7 @@ void _logica5x5() {
     }
 }
 
-void _logics4x4() {
+void _logics4x4() { // 플레이 중 아래로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (4x4용)
     while (1) {
         for (int i = 2; i >= 0; i--) {
             for (int j = 0; j < 4; j++) {
@@ -484,7 +483,7 @@ void _logics4x4() {
     }
 }
 
-void _logics5x5() {
+void _logics5x5() { // 플레이 중 아래로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (5x5용)
     while (1) {
         for (int i = 3; i >= 0; i--) {
             for (int j = 0; j < 5; j++) {
@@ -522,7 +521,7 @@ void _logics5x5() {
     }
 }
 
-void _logicd4x4() {
+void _logicd4x4() { // 플레이 중 오른쪽으로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (4x4용)
     while (1) {
         for (int i = 0; i < 4; i++) {
             for (int j = 2; j >= 0; j--) {
@@ -559,8 +558,8 @@ void _logicd4x4() {
         }
     }
 }
-
-void _logicd5x5() {
+ 
+void _logicd5x5() { // 플레이 중 아래로 가는 키를 눌렀을 때의 숫자들의 이동 논리를 시행하는 함수 (5x5용)
     while (1) {
         for (int i = 0; i < 5; i++) {
             for (int j = 3; j >= 0; j--) {
@@ -598,7 +597,7 @@ void _logicd5x5() {
     }
 }
 
-void _logicaddnewnum4x4() {
+void _logicaddnewnum4x4() { // 플레이 중 칸이 움직이거나, 초기에 새로운 숫자 블럭을 생성하는 함수 (4x4용)
     int n = rand() % 10;
     int howmanyadd;
     if (0 <= n && n <= 6) howmanyadd = 1;
@@ -614,7 +613,7 @@ void _logicaddnewnum4x4() {
     arr4[newpos1 / 4][newpos1 % 4] = newnum1;
 }
 
-void _logicaddnewnum5x5() {
+void _logicaddnewnum5x5() { // 플레이 중 칸이 움직이거나, 초기에 새로운 숫자 블럭을 생성하는 함수 (5x5용)
     int n = rand() % 10;
     int howmanyadd;
     if (0 <= n && n <= 6) howmanyadd = 1;
@@ -630,7 +629,7 @@ void _logicaddnewnum5x5() {
     arr5[newpos1 / 5][newpos1 % 5] = newnum1;
 }
 
-int _checkifclear4x4() {
+int _checkifclear4x4() { // 클리어 (2048인 블럭을 만들었을 때)를 확인하는 함수 (4x4용)
     int check = 0;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
@@ -643,7 +642,7 @@ int _checkifclear4x4() {
     else return 0;
 }
 
-int _checkifclear5x5() {
+int _checkifclear5x5() { // 클리어 (2048인 블럭을 만들었을 때)를 확인하는 함수 (5x5용)
     int check = 0;
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
@@ -656,7 +655,7 @@ int _checkifclear5x5() {
     else return 0;
 }
 
-int _checkifdead4x4() {
+int _checkifdead4x4() { //게임 오버 (더 이상 움직이지 못하는 상황)인지 판단하는 함수 (4x4용)
     int checkarr[5][5];
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
@@ -692,7 +691,7 @@ int _checkifdead4x4() {
     else return 0;
 }
 
-int _checkifdead5x5() {
+int _checkifdead5x5() { //게임 오버 (더 이상 움직이지 못하는 상황)인지 판단하는 함수 (5x5용)
     int checkarr[6][6];
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < 6; j++) {
@@ -728,7 +727,7 @@ int _checkifdead5x5() {
     else return 0;
 }
 
-void _printclearscreen4x4(int n) {
+void _printclearscreen4x4(int n) { //게임 완료 (수가 2048인 블럭을 만들었을) 시 결과화면을 보여주고, 다시할지 물어보는 함수 (4x4 용)
     system("mode con: cols=180 lines=60");
     system("cls");
     gotoxy(45, 5);
@@ -763,10 +762,25 @@ void _printclearscreen4x4(int n) {
     printf("└──────────────────────────────────────────────────────────┘\n");
     gotoxy(72, 19);
     printf("Your Score : %05d | Best Score : %05d", score4x4, highscore4x4);
-    Sleep(10000);
+    char inputkey;
+    gotoxy(75, 35);
+    printf("Press 'y' to retry, 'n' to End : ");
+    while (1) {
+        inputkey = _getch();
+        if (inputkey == 'y') {
+            retry = 1;
+            break;
+        }
+        else if (inputkey == 'n') {
+            retry = 0;
+            break;
+        }
+        else continue;
+    }
+    system("cls");
 }
 
-void _printclearscreen5x5(int n) {
+void _printclearscreen5x5(int n) { //게임 완료 (수가 2048인 블럭을 만들었을) 시 결과화면을 보여주고, 다시할지 물어보는 함수 (5x5 용)
     system("mode con: cols=180 lines=60");
     system("cls");
     gotoxy(45, 5);
@@ -801,10 +815,25 @@ void _printclearscreen5x5(int n) {
     printf("└──────────────────────────────────────────────────────────┘\n");
     gotoxy(72, 19);
     printf("Your Score : %05d | Best Score : %05d", score5x5, highscore5x5);
-    Sleep(10000);
+    char inputkey;
+    gotoxy(75, 35);
+    printf("Press 'y' to retry, 'n' to End : ");
+    while (1) {
+        inputkey = _getch();
+        if (inputkey == 'y') {
+            retry = 1;
+            break;
+        }
+        else if (inputkey == 'n') {
+            retry = 0;
+            break;
+        }
+        else continue;
+    }
+    system("cls");
 }
 
-void _printdeadscreen4x4(int n) {
+void _printdeadscreen4x4(int n) { //게임 실패 시 게임 오버창과 함께 다시 플레이 할지 물어보는 함수 (4x4용)
     system("mode con: cols=180 lines=60");
     system("cls");
     gotoxy(45, 5);
@@ -881,7 +910,7 @@ void _printdeadscreen4x4(int n) {
     system("cls");
 }   
 
-void _printdeadscreen5x5(int n) {
+void _printdeadscreen5x5(int n) { //게임 실패 시 게임 오버창과 함께 다시 플레이 할지 물어보는 함수 (5x5용)
     system("mode con: cols=180 lines=60");
     system("cls");
     gotoxy(45, 5);
@@ -958,7 +987,7 @@ void _printdeadscreen5x5(int n) {
     system("cls");
 }
 
-void _printdescription4x4() {
+void _printdescription4x4() { //게임 중, 본인의 점수와 최고 점수를 보여주는 함수 (4x4용)
 
     gotoxy(0, 30);
     _printRGBColoredString(205, 193, 180, "┌──────────────────────────────────────────────────────────┐\n");
@@ -982,7 +1011,7 @@ void _printdescription4x4() {
     printf("Your Score : %05d | Best Score : %05d", score4x4,highscore4x4);
 }
 
-void _printdescription5x5() {
+void _printdescription5x5() { //게임 중, 본인의 점수와 최고 점수를 보여주는 함수 (5x5 용)
     gotoxy(0, 38);
     _printRGBColoredString(205, 193, 180, "┌────────────────────────────────────────────────────────────────────────┐\n");
     gotoxy(0, 39);
@@ -1005,19 +1034,19 @@ void _printdescription5x5() {
     printf("Your Score : %05d | Best Score : %05d", score5x5, highscore5x5);
 }
 
-void _print4x4() {
+void _print4x4() { //게임을 플레이 할 때 출력에 관련된 함수를 모두 순차적으로 실행하는 함수 (4x4용)
     _saveimagenames4x4();
     _printdescription4x4();
     _displayimages4x4();
 }
 
-void _print5x5() {
+void _print5x5() { //게임을 플레이 할 때 출력에 관련된 함수를 모두 순차적으로 실행하는 함수 (5x5용)
     _saveimagenames5x5();
     _printdescription5x5();
     _displayimages5x5();
     //flag
 }
-void _play4x4() {
+void _play4x4() { // 게임 플레이 시 전체적인 순서에 따라 동작하며 다른 함수를 호출하는 메인함수 (4x4용)
     system("mode con: cols=61 lines=40");
     int rowstart = 30;
     int rowend = 112;
@@ -1066,7 +1095,7 @@ void _play4x4() {
         }
     }
 }
-void _play5x5() {
+void _play5x5() {  // 게임 플레이 시 전체적인 순서에 따라 동작하며 다른 함수를 호출하는 메인함수 (5x5용)
     system("mode con: cols=76 lines=48");
     int rowstart = 30;
     int rowend = 112;
@@ -1116,7 +1145,7 @@ void _play5x5() {
     }
 }
 
-int main() {
+int main() { //main함수 (프로세스 이름, 콘솔창 크기, 배경색/글자색, 노래 재생)
     srand(time(NULL));
     system("title 2048_in_C"); // 프로세스 이름
     system("mode con: cols=180 lines=60"); // !콘솔창 크기
